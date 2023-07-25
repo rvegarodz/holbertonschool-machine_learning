882993e7a021836a83325edf068d3f4a19904ff7
rvegarodz <r.vega27@outlook.com>
2023-07-25 10:00:34 -0400

    adding file for task 6

---
+++ math/linear_algebra/6-howdy_partner.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+""" This file contains the function called cat_arrays() """
+
+
+def cat_arrays(arr1, arr2):
+    """
+    Function that concatenates two arrays.
+    Arguments:
+        arr1: list
+        arr2: list
+    Returns: new list
+    """
+    nw_list = arr1 + arr2
+    return nw_list
