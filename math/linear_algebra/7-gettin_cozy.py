8e32add677c8eb7ec36f0c54ee073e1cffbb95c3
rvegarodz <r.vega27@outlook.com>
2023-07-26 15:58:34 -0400

    adding file for task 7

---
+++ math/linear_algebra/7-gettin_cozy.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+""" This file contains the function called cat_arrays() """
+
+
+def cat_matrices2D(mat1, mat2, axis=0):
+    """
+    Function that concatenates two matrices along a specific axis.
+    Arguments:
+        mat1: nested list
+        mat2: nested list
+        axis: specific axis
+    Returns: new matrix
+    """
+    if (axis == 0):
+        nw_matrix = []
+        for i in mat1:
+            nw_matrix.append(i)
+        for i in mat2:
+            nw_matrix.append(i)
+        return nw_matrix
+    else:
+        nw_matrix = []
+        if (len(mat1) == len(mat2)):
+            for i in range(len(mat1)):
+                nw_matrix.append(mat1[i] + mat2[i])
+            return nw_matrix
