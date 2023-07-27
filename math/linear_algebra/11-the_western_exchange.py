5e5c44885c99cba705ae077cfb7f1f6636a5808c
rvegarodz <r.vega27@outlook.com>
2023-07-27 16:34:26 -0400

    removing import

--- math/linear_algebra/11-the_western_exchange.py
+++ math/linear_algebra/11-the_western_exchange.py
@@ -1,8 +1,7 @@
 #!/usr/bin/env python3
 """ This file contains the function called np_transpose() """
-import numpy as np
 
 
 def np_transpose(matrix):
     """ Function that transposes matrix """
-    return np.transpose(matrix)
+    return matrix.T
