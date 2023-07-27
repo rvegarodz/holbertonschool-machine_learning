39abf93530b222d87aed9699b82d7c900befb8e1
rvegarodz <r.vega27@outlook.com>
2023-07-27 16:06:55 -0400

    adding files for task 11

---
+++ math/linear_algebra/11-the_western_exchange.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+""" This file contains the function called np_transpose() """
+import numpy as np
+
+
+def np_transpose(matrix):
+    """ Function that transposes matrix """
+    return np.transpose(matrix)
