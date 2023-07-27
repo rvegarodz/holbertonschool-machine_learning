ba07752cbe27235c1c95a787ca44c841e113293d
rvegarodz <r.vega27@outlook.com>
2023-07-27 15:57:02 -0400

    adding files for task 10

---
+++ math/linear_algebra/10-ill_use_my_scale.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+""" This file contains the function called np_shape( """
+import numpy as np
+
+def np_shape(matrix):
+    """ Function that calculates the shape of a numpy.ndarray """
+    return np.shape(matrix)
