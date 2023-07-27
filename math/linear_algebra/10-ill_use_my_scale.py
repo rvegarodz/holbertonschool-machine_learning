66b8ad1b620d8171dde995271ae12bd94e1ddaa1
rvegarodz <r.vega27@outlook.com>
2023-07-27 15:57:34 -0400

    fixing pepocho

--- math/linear_algebra/10-ill_use_my_scale.py
+++ math/linear_algebra/10-ill_use_my_scale.py
@@ -2,6 +2,7 @@
 """ This file contains the function called np_shape( """
 import numpy as np
 
+
 def np_shape(matrix):
     """ Function that calculates the shape of a numpy.ndarray """
     return np.shape(matrix)
