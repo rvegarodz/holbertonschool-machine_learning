15c7875a5ac5b56ea079edc81dc72d90e285d6de
rvegarodz <r.vega27@outlook.com>
2023-07-27 16:29:05 -0400

    adding files for task 14

---
+++ math/linear_algebra/14-saddle_up.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+""" This file contains the function called np_matmul() """
+import numpy as np
+
+
+def np_matmul(mat1, mat2):
+    """ Function that performs matrix multiplication """
+    return np.matmul(mat1, mat2)
