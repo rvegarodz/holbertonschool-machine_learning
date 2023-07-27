850b60483bbcb822d80874ec756c12daaec27dfe
rvegarodz <r.vega27@outlook.com>
2023-07-27 16:20:03 -0400

    removing import

--- math/linear_algebra/12-bracin_the_elements.py
+++ math/linear_algebra/12-bracin_the_elements.py
@@ -1,13 +1,12 @@
 #!/usr/bin/env python3
 """ This file contains the function called np_elementwise() """
-import numpy as np
 
 
 def np_elementwise(mat1, mat2):
     """ Function that performs element-wise addition,
     subtraction, multiplication, and division """
-    add = np.add(mat1, mat2)
-    sub = np.subtract(mat1, mat2)
-    mul = np.multiply(mat1, mat2)
-    div = np.divide(mat1, mat2)
+    add = mat1 + mat2
+    sub = mat1 - mat2
+    mul = mat1 * mat2
+    div = mat1 / mat2
     return (add, sub, mul, div)
