f07addbad3a9bc845f6408a0531781be9957a88c
rvegarodz <r.vega27@outlook.com>
2023-08-06 17:51:23 -0400

    adding file for task 10

---
+++ math/calculus/10-matisse.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+""" File that contians the function poly_derivative"""
+
+
+def poly_derivative(poly):
+    """ Function that calculates the derivative of a polynomial"""
+    if (type(poly) == list and len(poly) >= 0):
+        result = []
+        lenn = len(poly)
+        if (lenn <= 1):
+            return result
+        else:
+            for i in range(lenn):
+                if i == 0:
+                    continue
+                elif i == 1:
+                    result.append(poly[i] * i)
+                else:
+                    result.append(poly[i] * i)
+            return result
