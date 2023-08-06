ba6cca9212ec9113009e53793d2ce82562165b1b
rvegarodz <r.vega27@outlook.com>
2023-08-06 12:03:28 -0400

    trying

--- math/calculus/9-sum_total.py
+++ math/calculus/9-sum_total.py
@@ -6,8 +6,10 @@ import numpy as np
 def summation_i_squared(n):
     """ Function that sum """
 
-    if (n):
+    if (type(n) is int):
         serie = np.arange(1, n + 1)
         serie_pow = np.power(serie, 2)
         result = np.sum(serie_pow)
         return result
+    else:
+        return None
