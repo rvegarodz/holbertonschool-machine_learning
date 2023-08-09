666d1bf67cadf8fc6fcdf8c8896dc28f233be616
rvegarodz <r.vega27@outlook.com>
2023-08-09 11:09:56 -0400

    adding files and notes of task 0

---
+++ math/probability/poisson.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+"This file have the class Poisson()"
+
+
+class Poisson():
+    "This class represent a poisson distribution"
+
+    def __init__(self, data=None, lambtha=1.):
+        pi = 3.1415926536
+        e = 2.7182818285
+
+        if data and data is not None:
+            if type(data) != list:
+                raise TypeError("data must be a list")
+            elif len(data) < 2:
+                raise ValueError("data must contain multiple values")
+            else:
+                mean = sum(item for item in data) / len(data)
+                self.lambtha = float(mean)
+        else:
+            if lambtha <= 0:
+                raise ValueError("lambtha must be a positive value")
+            else:
+                self.lambtha = float(lambtha)
