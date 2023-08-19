fb461e25b29459c2259f870325896318e11a7e05
rvegarodz <r.vega27@outlook.com>
2023-08-19 13:42:51 -0400

    documantation

--- supervised_learning/classification/0-neuron.py
+++ supervised_learning/classification/0-neuron.py
@@ -1,13 +1,12 @@
 #!/usr/bin/env python3
 import numpy as np
-"File that contain Neuron class"
+"""File that contain Neuron class"""
 
 
 class Neuron():
-    "Class that defines a single neuron performing binary classification"
+    """Class that defines a single neuron performing binary classification"""
 
     def __init__(self, nx):
-        "Init of the Neuron class"
         if type(nx) != int:
             raise TypeError("nx must be an integer")
         elif nx < 1:
