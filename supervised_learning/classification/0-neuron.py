f0f515fe67652e8895c8cc2d4906af865f758e31
rvegarodz <r.vega27@outlook.com>
2023-08-19 11:14:12 -0400

    adding file for task 0

---
+++ supervised_learning/classification/0-neuron.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+import numpy as np
+
+
+class Neuron():
+    "Class that defines a single neuron performing binary classification"
+
+    def __init__(self, nx):
+        if type(nx) != int:
+            raise TypeError("nx must be an integer")
+        elif nx < 1:
+            raise TypeError("nx must be a positive integer")
+        else:
+            self.W = np.random.normal(size=(1, nx))
+            self.b = 0
+            self.A = 0
