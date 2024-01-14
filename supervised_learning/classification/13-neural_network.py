ba8ce561c0570866a20cb74dc8e2f6601fda5445
rvegarodz <r.vega27@outlook.com>
2024-01-14 12:22:48 -0400

    adding file

--- supervised_learning/classification/13-neural_network.py
+++ supervised_learning/classification/13-neural_network.py
@@ -134,23 +134,3 @@ class NeuralNetwork:
         self.__b1 = self.__b1 - alpha * db1
         self.__W2 = self.__W2 - alpha * dw2
         self.__b2 = self.__b2 - alpha * db2
-
-    def train(self, X, Y, iterations=5000, alpha=0.05):
-        """
-            Trains the neural network
-        """
-        if type(iterations) is not int:
-            raise TypeError("iterations must be an integer")
-
-        if iterations < 0:
-            raise ValueError("iterations must be a positive integer")
-
-        if type(alpha) is not float:
-            raise TypeError("alpha must be a float")
-
-        if alpha < 0:
-            raise ValueError("alpha must be positive")
-        for train in range(iterations):
-            self.forward_prop(X)
-            self.gradient_descent(X, Y, self.__A1, self.__A2, alpha)
-        return self.evaluate(X, Y)
