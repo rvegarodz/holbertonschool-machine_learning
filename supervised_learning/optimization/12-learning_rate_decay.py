c44c9a592432132c876e51d81af4b92c58b6ba14
rvegarodz <r.vega27@outlook.com>
2024-01-28 19:19:45 -0400

    adding files

---
+++ supervised_learning/optimization/12-learning_rate_decay.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""
+module learning_rate_decay
+"""
+import tensorflow.compat.v1 as tf
+
+
+def learning_rate_decay(alpha, decay_rate, global_step, decay_step):
+    """
+    creates a learning rate decay operation in tensorflow using inverse time
+    decay
+    """
+    return tf.train.inverse_time_decay(alpha, global_step, decay_step,
+                                       decay_rate, staircase=True)
