a48f6a8681283fdd40bd47de6bcc11888293d49b
rvegarodz <r.vega27@outlook.com>
2024-02-11 18:31:59 -0400

    pepocho again

--- supervised_learning/cnn/0-conv_forward.py
+++ supervised_learning/cnn/0-conv_forward.py
@@ -5,7 +5,7 @@ import numpy as np
 
 def conv_forward(A_prev, W, b, activation,
                  padding="same", stride=(1, 1)):
-    """Function that performs forward propagation 
+    """Function that performs forward propagation
     over a convolutional layer of a neural network"""
     (m, h_prev, w_prev, c_prev) = A_prev.shape
     (kh, kw, c_prev, c_new) = W.shape
